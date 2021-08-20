d4d1eb08af8aa0145d09e6c4f1339856076b3ca0
MN0607 <kobukuro.mun@gmail.com>
2021-08-20 11:07:41 +0900

    編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   root to: 'designs#index'
 
-  resources :designs, only: [:index, :new, :create, :show] do
+  resources :designs, only: [:index, :new, :create, :show, :edit, :update] do
     collection do
       get 'index_co'
       get 'mypage'
